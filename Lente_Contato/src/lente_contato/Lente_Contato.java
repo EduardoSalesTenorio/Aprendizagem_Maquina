e90fd99dc97607c493cb0a3a626e52c478c79bb2
°° <@192.168.0.104>
2018-07-02 06:19:37 -0300

    Iniciando

--- Lente_Contato/src/lente_contato/Lente_Contato.java
+++ Lente_Contato/src/lente_contato/Lente_Contato.java
@@ -3,11 +3,9 @@ package lente_contato;
 
 public class Lente_Contato {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
